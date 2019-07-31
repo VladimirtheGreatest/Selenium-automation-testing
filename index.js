4053bbcea366ace2c2242e419143c99781edda46
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-07-31 19:48:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,38 @@ const driver = new selenium.Builder().forBrowser("chrome").build();
 
 driver.get(URL);
 
+const invitees = [
+   'Gonzalo Torres del Fierro',
+   'Shadd Anderson',
+   'George Aparece',
+   'Shadab Khan',
+   'Joseph Michael Casey',
+   'Jennifer Nordell',
+   'Faisal Albinali',
+   'Taron Foxworth',
+   'David Riesz',
+   'Maicej Torbus',
+   'Martin Luckett',
+   'Joel Bardsley',
+   'Reuben Varzea',
+   'Ken Alger',
+   'Amrit Pandey',
+   'Rafal Rudzinski',
+   'Brian Lynch',
+   'Lupe Camacho',
+   'Luke Fiji',
+   'Sean Christensen',
+   'Philip Graf',
+   'Mike Norman',
+   'Michael Hulet',
+   'Brent Suggs'
+];
+
 const locators = {
     inviteeForm : By.id("registrar"),
-    inviteeNameField : By.css("#registrar input[name='name']")
+    inviteeNameField : By.css("#registrar input[name='name']"),
+    toggleNonRespondersVisibility : By.css(".main > div  input"),
+    removeButtonForInvitee : invitee => By.xpath(`//span[text() = "${invitee}"]//../button[last()]`)
 };
 
 function addInvitee(name){
@@ -16,6 +45,17 @@ function addInvitee(name){
   driver.findElement(locators.inviteeForm).submit();
 }
 
-addInvitee("joseph Smith");
-addInvitee("Boris Johnson");
+function removeInvitee(invitee){
+  driver.findElement(locators.removeButtonForInvitee(invitee)).click();
+}
+
+function toggleNonRespondersVisibility(){
+  driver.findElement(locators.toggleNonRespondersVisibility).click();
+}
+
+//invitees.forEach(invitee => addInvitee(invitee));
+invitees.forEach(addInvitee);
+removeInvitee("Shadd Anderson");
+
+//toggleNonRespondersVisibility();
 
