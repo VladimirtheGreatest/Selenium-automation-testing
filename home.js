0ebbd1b91e7e8b9ed0e33366c9aee4a9054345c2
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-08-01 19:07:32 +0100

    Update home.js

--- home.js
+++ home.js
@@ -8,7 +8,7 @@ class HomePage {
         inviteeForm : By.id("registrar"),
         inviteeNameField : By.css("#registrar input[name='name']"),
         toggleNonRespondersVisibility : By.css(".main > div  input"),
-        removeButtonForInvitee : invitee => By.xpath(`//span[text() = "${invitee}"]//../button[last()]`)
+        inviteeByName : name => By.xpath(`//span[text() = "${name}"]//..`), // check the new invitee name
     };
   }
   open(){
@@ -20,14 +20,33 @@ class HomePage {
     this.driver.findElement(this.locators.inviteeForm).submit();
   }
 
-   removeInvitee(invitee){
-    this.driver.findElement(this.locators.removeButtonForInvitee(invitee)).click();
-  }
 
    toggleNonRespondersVisibility(){
     this.driver.findElement(this.locators.toggleNonRespondersVisibility).click();
   }
+
+  findInviteeByName(name){
+    const el = this.driver.findElement(this.locators.inviteeByName(name));
+    return new Invitee(el);  //create new class check the bottom
+  }
 }
+
+class Invitee {
+  constructor(element){
+    this.element = element;
+    this.locators = {
+      removeButton : By.css("button:last-child"),
+      confirmedCheckbox : By.css("input[type='checkbox']")
+    };
+  }
+  remove(){
+    this.element.findElement(this.locators.removeButton).click();
+  }
+  toggleConfirmation(){
+    this.element.findElement(this.locators.confirmedCheckbox).click();
+  }
+}
+
 
 
 module.exports = HomePage;
