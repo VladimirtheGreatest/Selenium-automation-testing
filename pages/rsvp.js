0e92f62be0401de3299ba84980670a8ecb9add9d
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-08-05 10:35:33 +0100

    Add files via upload

---
+++ pages/rsvp.js
@@ -0,0 +1,23 @@
+//Page object class, "model"
+require('chromedriver');
+const {Browser, By, Key, until} = require("selenium-webdriver");
+const url = "https://treehouse-projects.github.io/selenium-webdriver-intermediate/waits/app/index.html";
+
+class RsvpPage {
+  constructor(driver){
+    this.driver = driver;
+    this.locators = {
+      invitedList : By.id('invitedList'),
+      registrationForm : By.id('registrar'),
+      invitees : By.css('#invitedList li')
+    }
+  }
+  //methods
+  open(){
+    this.driver.get(url);
+  }
+}
+
+
+
+module.exports = RsvpPage;
