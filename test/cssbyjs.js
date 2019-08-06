167845a2bc12dde539a4d2c68a26383856da57a6
Vladimir <48209956+VladimirtheGreatest@users.noreply.github.com>
2019-08-06 07:19:27 +0100

    Add files via upload

---
+++ test/cssbyjs.js
@@ -0,0 +1,30 @@
+require('chromedriver');
+const {Browser, By, Key, until} = require("selenium-webdriver");
+const {suite} = require("selenium-webdriver/testing");
+const assert = require('assert');
+const ButtonsPage = require('../pages/cssbyjs.js');
+
+suite(function(env) {
+    describe('Save button', function() {
+        this.timeout(10000);
+        let driver;
+        let page;
+
+        before(async function() {
+            driver = await env.builder().build();
+            page = new ButtonsPage(driver);
+            await page.open();
+        });
+
+        it('Updates status text', async function() {
+            await page.clickSave();
+            let span = await driver.findElement(page.locators.status);
+            let text = await span.getText();
+            assert(text.includes("Saved!"));
+        });
+
+        after(async function() {
+            driver.quit();
+        });
+    });
+});
